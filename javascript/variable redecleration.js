ae2ab0f15f73cdec071c76fae37f91f8f3b780e7
suryaprakash <suryaprakash2732006@gmail.com>
2025-08-06 21:38:56 +0530

    commit

---
+++ javascript/variable redecleration.js
@@ -0,0 +1,22 @@
+//var 
+
+var a=21;
+console.log(a);
+var a=29;
+console.log(a);  //we can redeclare or reassign the values assigned in var.it does not shoe error.
+
+//let
+
+let a=54;
+console.log(a);
+let a=90;
+console.log(a);  //here we cantredeclare the value assigned in let.it shows error
+
+
+
+//const
+
+const a=33;
+console.log(a);
+const a=22;
+console.log(a);  //here also we cant redeclare the value assigned in let.it shows error
