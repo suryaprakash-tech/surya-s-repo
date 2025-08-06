9a337aad80b7be7b57f3297764650bc1cce85ed6
suryaprakash <suryaprakash2732006@gmail.com>
2025-08-06 22:51:39 +0530

    commit

---
+++ javascript/value assignment.js
@@ -0,0 +1,23 @@
+//var 
+var a=30;
+console.log(a);
+a=56;
+console.log(a);  //we can reassign the values in 'var'.
+
+
+//let 
+
+let b= 22;
+console.log(b);
+b=33;
+console.log(b);  //here 'let' also allows us to reassign the values
+
+
+//const
+
+const c=23;
+console.log(c);
+c=43;
+console.log(c);  //here in 'const' we cant reassign the values  
+
+
