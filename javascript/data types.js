73e10453ba0b914a450c832ed657ecfe8241b45e
suryaprakash <suryaprakash2732006@gmail.com>
2025-08-07 23:04:11 +0530

    commit

--- javascript/data types.js
+++ javascript/data types.js
@@ -10,3 +10,14 @@ console.log(typeof b);
 console.log(typeof c);
 console.log(typeof d);
 console.log(typeof e);
+
+//USE OF SYMBOL() --> it generates a unique value .dont generate the same values
+
+const s1=Symbol();    
+//console.log(s1);
+const s2=Symbol();
+//console.log(s2);
+console.log(s1==s2);   //therefore it shows false
+
+
+
