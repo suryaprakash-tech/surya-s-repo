a7de6a01d2e77d3d2552eb391b6de6e5eee5ceae
suryaprakash <suryaprakash2732006@gmail.com>
2025-08-07 22:58:18 +0530

    commit

---
+++ javascript/data types.js
@@ -0,0 +1,12 @@
+//primitive data type(inbuilt)
+
+let a=20;
+let b="surya";
+let c=true;
+let d;
+let e=null;
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+console.log(typeof d);
+console.log(typeof e);
