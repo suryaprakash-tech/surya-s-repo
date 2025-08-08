88f3ac606f5e7341cf06c03dbd9e84fc5d07c7f9
suryaprakash <suryaprakash2732006@gmail.com>
2025-08-08 20:50:55 +0530

    commit

--- javascript/type conversion.js
+++ javascript/type conversion.js
@@ -4,8 +4,13 @@
 
 let a=123;
 console.log(a,typeof a);
-a=String(a);
+a=String(a);                  //we can also use a.toString();
 console.log(typeof a);
 
 
 //2.string to number
+
+let b='123';
+console.log(b,typeof b);
+b=Number(b);
+console.log(typeof b);
