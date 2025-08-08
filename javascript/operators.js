6aad9dd7bf7a40971792f86c1b3283154fe07c83
suryaprakash <suryaprakash2732006@gmail.com>
2025-08-08 21:12:22 +0530

    operator

---
+++ javascript/operators.js
@@ -0,0 +1,23 @@
+//arithmetic operator
+
+let a=20;
+let b=10;
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+console.log(a**b);
+
+console.log(a++);
+console.log(a)
+console.log(b--);
+console.log(b); 
+
+
+console.log(a)
+console.log(++a);
+console.log(a);
+console.log(b);
+console.log(--b);
+console.log(b);
